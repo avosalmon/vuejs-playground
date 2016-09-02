3e0119e3b2db03956ca721075f1ffd5bee3225af
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2016-09-02 16:34:59 +0900

    implement index.html

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,23 @@
 var data = {
-    message: 'Hello Vue.js!!'
+    input: 'Hello Vue.js!!',
+    messages: []
+};
+
+var methods = {
+    addMessage: function() {
+        var message = this.input.trim();
+        if (message) {
+            this.messages.push(message);
+            this.input = '';
+        }
+    },
+    removeMessage: function(index) {
+        this.messages.splice(index, 1);
+    }
 };
 
 new Vue({
     el: '#app',
-    data: data
+    data: data,
+    methods: methods
 });
